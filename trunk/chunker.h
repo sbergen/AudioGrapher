5d93b8befd337a2d91d7d7127a55dc50115faf78
sbergen <sbergen@f9b19b71-d223-49cf-84ce-4870ce8e5f52>
2009-11-01 19:55:35 +0000

    Added chunker

---
+++ trunk/chunker.h
@@ -0,0 +1,51 @@
+#ifndef AUDIOGRAPHER_CHUNKER_H
+#define AUDIOGRAPHER_CHUNKER_H
+
+#include "vertex.h"
+#include <cstring>
+
+namespace AudioGrapher
+{
+
+template<typename T>
+class Chunker : public Vertex<T, T>
+{
+  public:
+	Chunker (nframes_t chunk_size)
+	  : chunk_size (chunk_size)
+	  , position (0)
+	{
+		buffer = new T[chunk_size];
+	}
+	
+	~Chunker()
+	{
+		delete [] buffer;
+	}
+	
+	void process (T * data, nframes_t frames)
+	{
+		if (position + frames < chunk_size) {
+			memccpy (&buffer[position], data, frames);
+			position += frames;
+		} else {
+			nframes_t const frames_to_copy = chunk_size - position;
+			memccpy (&buffer[position], data, frames_to_copy);
+			Vertex<T, T>::output (buffer, chunk_size);
+			
+			memccpy (buffer, &data[frames_to_copy], frames - frames_to_copy);
+			position =  frames - frames_to_copy;
+		}
+	}
+	
+  private:
+	nframes_t chunk_size;
+	nframes_t position;
+	T * buffer;
+	
+};
+
+} // namespace
+
+#endif // AUDIOGRAPHER_CHUNKER_H
+
