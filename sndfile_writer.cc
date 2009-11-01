b5324d65083f7fd9133ea059b6748e6e89ad94b7
sbergen <sbergen@f9b19b71-d223-49cf-84ce-4870ce8e5f52>
2009-11-01 19:25:40 +0000

    Added sndfile writer

---
+++ sndfile_writer.cc
@@ -0,0 +1,110 @@
+#include "sndfile_writer.h"
+#include "exception.h"
+
+#include <string.h>
+
+#include <boost/format.hpp>
+
+namespace AudioGrapher
+{
+
+using std::string;
+using boost::str;
+using boost::format;
+
+/* SndfileWriterBase */
+
+SndfileWriterBase::SndfileWriterBase (int channels, nframes_t samplerate, int format, string const & path)
+  : path (path)
+  , end_of_input (false)
+{
+	char errbuf[256];
+
+	sf_info.channels = channels;
+	sf_info.samplerate = samplerate;
+	sf_info.format = format;
+
+	if (!sf_format_check (&sf_info)) {
+		throw Exception ("Invalid format given for SndfileWriter!");
+	}
+
+	if (path.length() == 0) {
+		throw Exception ("No output file specified for SndFileWriter");
+	}
+
+	/* TODO add checks that the directory path exists, and also
+	   check if we are overwriting an existing file...
+	*/
+
+	// Open file TODO make sure we have enough disk space for the output
+	if (path.compare ("temp")) {
+		if ((sndfile = sf_open (path.c_str(), SFM_WRITE, &sf_info)) == 0) {
+			sf_error_str (0, errbuf, sizeof (errbuf) - 1);
+			throw Exception (str (boost::format ("Cannot open output file \"%1%\" for SndFileWriter (%2%)") % path % errbuf));
+		}
+	} else {
+		FILE * file;
+		if (!(file = tmpfile ())) {
+			throw Exception ("Cannot open tempfile");
+		}
+		sndfile = sf_open_fd (fileno(file), SFM_RDWR, &sf_info, true);
+	}
+}
+
+SndfileWriterBase::~SndfileWriterBase ()
+{
+	sf_close (sndfile);
+}
+
+/* SndfileWriter */
+
+template <typename T>
+SndfileWriter<T>::SndfileWriter (int channels, nframes_t samplerate, int format, string const & path) :
+  SndfileWriterBase (channels, samplerate, format, path)
+{
+	// init write function
+	init ();
+}
+
+template <>
+void
+SndfileWriter<float>::init ()
+{
+	process_func = &sf_writef_float;
+}
+
+template <>
+void
+SndfileWriter<int>::init ()
+{
+	process_func = &sf_writef_int;
+}
+
+template <>
+void
+SndfileWriter<short>::init ()
+{
+	process_func = &sf_writef_short;
+}
+
+template <typename T>
+void
+SndfileWriter<T>::process (T * data, nframes_t frames)
+{
+	char errbuf[256];
+	nframes_t written = (*process_func) (sndfile, data, frames);
+	if (written != frames) {
+		sf_error_str (sndfile, errbuf, sizeof (errbuf) - 1);
+		throw Exception (str ( format("Could not write data to output file (%1%)") % errbuf));
+	}
+
+	if (end_of_input) {
+		sf_write_sync (sndfile);
+	}
+}
+
+template class SndfileWriter<short>;
+template class SndfileWriter<int>;
+template class SndfileWriter<float>;
+
+} // namespace
---
+++ sndfile_writer.h
@@ -0,0 +1,50 @@
+#ifndef AUDIOGRAPHER_SNDFILE_WRITER_H
+#define AUDIOGRAPHER_SNDFILE_WRITER_H
+
+#include <sndfile.h>
+#include <string>
+
+#include "types.h"
+#include "sink.h"
+
+namespace AudioGrapher
+{
+
+/// Common interface for templated libsndfile writers
+class SndfileWriterBase
+{
+  public:
+
+	void set_end_of_input () { end_of_input = true; }
+	SNDFILE * get_sndfile () const { return sndfile; }
+
+  protected:
+	SndfileWriterBase (int channels, nframes_t samplerate, int format, std::string const & path);
+	virtual ~SndfileWriterBase ();
+
+	std::string    path;
+	SF_INFO        sf_info;
+	SNDFILE *      sndfile;
+	bool           end_of_input;
+};
+
+
+/// Template parameter specific parts of sndfile writer
+template <typename T>
+class SndfileWriter : public SndfileWriterBase, public Sink<T>
+{
+	SndfileWriter (int channels, nframes_t samplerate, int format, std::string const & path);
+	void process (T * data, nframes_t frames);
+	virtual ~SndfileWriter () {}
+
+  protected:
+
+	sf_count_t (*process_func)(SNDFILE *, const T *, sf_count_t);
+
+  private:
+	void init (); // Inits write function
+};
+
+} // namespace
+
+#endif // AUDIOGRAPHER_SNDFILE_WRITER_H
