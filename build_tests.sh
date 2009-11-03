b8767d2c01f93123faade92e09cf3ac226e32b59
sbergen <sbergen@f9b19b71-d223-49cf-84ce-4870ce8e5f52>
2009-11-03 14:31:55 +0000

    remove old compiling test, as it doesn't really do much...

--- build_tests.sh
+++
@@ -1,6 +0,0 @@
-g++ -o test `pkg-config --cflags --libs glibmm-2.4 gthread-2.0 sndfile samplerate` \
-gdither/gdither.cc \
-sample_format_converter.cc \
-sndfile_writer.cc \
-sr_converter.cc \
-test.cc
