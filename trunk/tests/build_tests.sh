3c375c3f85d1b846a0251142086b09f97ad5e3e1
sbergen <sbergen@f9b19b71-d223-49cf-84ce-4870ce8e5f52>
2009-11-03 15:34:26 +0000

    Added threader test, fixed threader + some random stuff again...

--- trunk/tests/build_tests.sh
+++ trunk/tests/build_tests.sh
@@ -1,9 +1,11 @@
 g++ -g -Wall -Wextra \
 -o test \
+`pkg-config --libs --cflags gthread-2.0 glibmm-2.4` \
 -ldl \
 `pkg-config --libs cppunit` \
 identity_vertex_test.cc \
 interleaver_test.cc \
 deinterleaver_test.cc \
 interleaver_deinterleaver_test.cc \
+threader_test.cc \
 test_runner.cc
--- trunk/threader.h
+++ trunk/threader.h
@@ -9,6 +9,7 @@
 
 #include "source.h"
 #include "sink.h"
+#include "exception.h"
 
 namespace AudioGrapher
 {
@@ -16,29 +17,44 @@ namespace AudioGrapher
 template <typename T>
 class Threader : public Source<T>, public Sink<T>
 {
+  private:
+	typedef std::vector<typename Source<T>::SinkPtr> OutputVec;
+
   public:
 	
-	Threader (Glib::ThreadPool & thread_pool)
+	Threader (Glib::ThreadPool & thread_pool, long wait_timeout_milliseconds = 1000)
 	  : thread_pool (thread_pool)
-	{}
+	  , readers (0)
+	  , wait_timeout (wait_timeout_milliseconds)
+	{ }
 	
 	virtual ~Threader () {}
 	
 	void add_output (typename Source<T>::SinkPtr output) { outputs.push_back (output); }
 	void clear_outputs () { outputs.clear (); }
-	void remove_output (typename Source<T>::SinkPtr output) { std::remove(outputs.begin(), outputs.end(), output); }
+	void remove_output (typename Source<T>::SinkPtr output) {
+		typename OutputVec::iterator new_end = std::remove(outputs.begin(), outputs.end(), output);
+		outputs.erase (new_end, outputs.end());
+	}
 	
 	void process (T * data, nframes_t frames)
 	{
+		wait_mutex.lock();
+		
 		unsigned int outs = outputs.size();
 		g_atomic_int_add (&readers, outs);
 		for (unsigned int i = 0; i < outs; ++i) {
 			thread_pool.push (sigc::bind (sigc::mem_fun (this, &Threader::process_channel), data, frames, i));
 		}
 		
-		wait_mutex.lock();
-		wait_cond.wait(wait_mutex);
+		Glib::TimeVal wait_time;
+		wait_time.assign_current_time();
+		wait_time.add_milliseconds(wait_timeout);
+		
+		wait_cond.timed_wait(wait_mutex, wait_time);
+		bool timed_out = (g_atomic_int_get (&readers) != 0);
 		wait_mutex.unlock();
+		if (timed_out) { throw Exception ("Threader: wait timed out"); }
 	}
 	
   private:
@@ -52,13 +68,13 @@ class Threader : public Source<T>, public Sink<T>
 		}
 	}
 
-	typedef std::vector<typename Source<T>::SinkPtr> OutputVec;
 	OutputVec outputs;
 
-	Glib::ThreadPool & thread_pool;	
+	Glib::ThreadPool & thread_pool;
 	Glib::Mutex wait_mutex;
 	Glib::Cond  wait_cond;
-	int         readers;
+	gint        readers;
+	long        wait_timeout;
 	
 	T * data;
 
