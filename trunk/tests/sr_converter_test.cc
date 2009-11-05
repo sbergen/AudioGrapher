036d4463dcb4abee47f39f72aa5a7172886d3ff0
sbergen <sbergen@f9b19b71-d223-49cf-84ce-4870ce8e5f52>
2009-11-05 11:46:05 +0000

    Make some changes to and tests for sr converter

--- trunk/tests/sr_converter_test.cc
+++ trunk/tests/sr_converter_test.cc
@@ -7,6 +7,8 @@ class SampleRateConverterTest : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE (SampleRateConverterTest);
   CPPUNIT_TEST (testNoConversion);
+  CPPUNIT_TEST (testUpsampleLength);
+  CPPUNIT_TEST (testDownsampleLength);
   CPPUNIT_TEST_SUITE_END ();
 
   public:
@@ -25,12 +27,12 @@ class SampleRateConverterTest : public CppUnit::TestFixture
 
 	void testNoConversion()
 	{
-		converter->init (44100, 44100);
-		converter->add_output (sink);
-		
 		assert (frames % 2 == 0);
 		nframes_t const half_frames = frames / 2;
 		nframes_t frames_output = 0;
+		
+		converter->init (44100, 44100);
+		converter->add_output (sink);
 		
 		converter->process (random_data, half_frames);
 		converter->set_end_of_input();
@@ -40,6 +42,44 @@ class SampleRateConverterTest : public CppUnit::TestFixture
 		CPPUNIT_ASSERT_EQUAL (frames, frames_output);
 		
 		CPPUNIT_ASSERT (Utils::array_equals (random_data, sink->get_array(), frames));
+	}
+
+	void testUpsampleLength()
+	{
+		assert (frames % 2 == 0);
+		nframes_t const half_frames = frames / 2;
+		nframes_t frames_output = 0;
+		
+		converter->init (44100, 88200);
+		converter->allocate_buffers (half_frames);
+		converter->add_output (sink);
+		
+		converter->process (random_data, half_frames);
+		converter->process (&random_data[half_frames], half_frames);
+		converter->set_end_of_input();
+
+		frames_output = sink->get_data().size();
+		nframes_t tolerance = 3;
+		CPPUNIT_ASSERT (2 * frames - tolerance < frames_output && frames_output < 2 * frames + tolerance);
+	}
+
+	void testDownsampleLength()
+	{
+		assert (frames % 2 == 0);
+		nframes_t const half_frames = frames / 2;
+		nframes_t frames_output = 0;
+		
+		converter->init (88200, 44100);
+		converter->allocate_buffers (half_frames);
+		converter->add_output (sink);
+		
+		converter->process (random_data, half_frames);
+		converter->process (&random_data[half_frames], half_frames);
+		converter->set_end_of_input();
+		
+		frames_output = sink->get_data().size();
+		nframes_t tolerance = 3;
+		CPPUNIT_ASSERT (half_frames - tolerance < frames_output && frames_output < half_frames + tolerance);
 	}
 	
 
